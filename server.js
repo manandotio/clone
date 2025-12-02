4bea97dfff603ee24ba8e917f46dbb029e1bdbc0
manandotio <mananb.msm@gmail.com>
2025-12-02 15:32:42 +0530

    abc

--- server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./src/app");
-const connectDB = require("./src/db/db");
-
-(async () => {
-  await connectDB();
-  app.listen(3000, () => console.log("Server running on port 3000"));
-})();
