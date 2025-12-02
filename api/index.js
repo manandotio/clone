aa3aadce3d1ba647b94f94b90bef1f8a50ea88fe
manandotio <mananb.msm@gmail.com>
2025-12-02 15:23:35 +0530

    abc

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
-const app = require("./src/app");
-const connectDB = require("./src/db/db");
+const serverless = require("serverless-http");
+const connectDB = require("../src/db/db");
+const app = require("../src/app");
+
+let cachedHandler = null;
 
 module.exports = async (req, res) => {
+  // Connect to MongoDB once (cached)
   await connectDB();
-  return app(req, res);
+
+  if (!cachedHandler) {
+    cachedHandler = serverless(app);
+  }
+
+  return cachedHandler(req, res);
 };
