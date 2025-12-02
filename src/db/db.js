cf646d96d97477aced0076e45cd65f233fb94526
manandotio <mananb.msm@gmail.com>
2025-12-02 15:59:48 +0530

    abc

--- src/db/db.js
+++ src/db/db.js
@@ -2,28 +2,14 @@ const mongoose = require("mongoose");
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) throw new Error("Missing MONGODB_URI");
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 async function connectDB() {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      bufferCommands: false,
-    }).then((mongoose) => {
-      console.log("MongoDB Connected");
-      return mongoose;
-    });
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
   }
-
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
 module.exports = connectDB;
